867859574e7384fd9f9de2c6e45ce8e37f1f70e6
Eman Johnson <emanjohnson91@gmail.com>
2019-04-09 23:14:24 +0200

    started on rbig implementation.

---
+++ layers/univariate.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+import torch.distributions.transforms as transform
+import torch.nn.functional as F
+import torch.distributions as distrib
+from torch.distributions import Uniform
+
+class UnivariateNormalize(nn.Module):
+    def __init__(self, histogram):
+        super(UnivariateNormalize, self).__init__()
+
+        # Register Parameters
+        self.regularize = nn.Parameter(torch.Tensor(1, 1))
+
+        self.build(histogram)
+        # # # Initialize Parameters
+        # self.init_parameters()
+
+    # def build(self, histogram):
+        
+    #     pass
+
+    # def _call(self, x):
+
+
+
+    #     return x
+
+    # def _fit(self, x):
+    #     # Check X
+
+    #     # Check Bounds
+
+    #     # Fit Histogram with Bin Edges
+
+
+    #     # ========================
+    #     # Regularization
+    #     # ========================
+
+
+    #     # Convert RV to rv distribution
+
+    #     return self
